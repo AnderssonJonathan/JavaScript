6421d17e865ad09b914376e244dc4a2176a9bced
MichaelGustavsson <michael.gustavsson@softtech-dev.se>
2023-04-17 16:46:21 +0200

    Added projects

--- 17-voting-app-maps/app.js
+++ 17-voting-app-maps/app.js
@@ -1,3 +1,57 @@
 /***********************************************************'*/
 // Voting app...
 /***********************************************************'*/
+const votes = new Map();
+votes.set('Titanic', 0);
+votes.set('Avatar', 0);
+votes.set('JurassicWorld', 0);
+votes.set('Gudfadern', 0);
+
+// lägger till en händelsehanterinf för submit händelsen i formuläret...
+document.querySelector('#vote-form').addEventListener('submit', vote);
+
+// Funktionen för att räkna röster...
+function vote(e) {
+  e.preventDefault();
+
+  const selectedOption = document.querySelector(
+    "input[name='vote-option']:checked"
+  );
+
+  // console.log(selectedOption);
+
+  if (!selectedOption) {
+    alert('Du måste välja film!');
+    return;
+  }
+
+  let voteCount = votes.get(selectedOption.value);
+  votes.set(selectedOption.value, voteCount + 1);
+
+  displayResult();
+
+  // Omöjligöra att rösta mer än en gång...
+  document
+    .querySelector('#vote-form')
+    .querySelectorAll('input')
+    .forEach((elem) => elem.setAttribute('disabled', true));
+}
+
+function displayResult() {
+  const result = document.querySelector('#result');
+  result.innerHTML = '';
+
+  for (let [option, voteCount] of votes) {
+    const elem = document.createElement('div');
+    elem.classList.add(
+      'flex',
+      'justify-content-between',
+      'px-2',
+      'py-1',
+      'border-bottom'
+    );
+
+    elem.innerHTML = `<strong>${option}: </strong> ${voteCount} röster`;
+    result.appendChild(elem);
+  }
+}
